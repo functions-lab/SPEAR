0072c6b49cef1a0b85d122282b048941cabcdbb3
Wei Cheng <wei.cheng@duke.edu>
2024-01-03 17:06:01 +0000

    feat-Refactor rfdc_task.py file

--- rfsoc_rfdc/rfdc_task.py
+++ rfsoc_rfdc/rfdc_task.py
@@ -4,99 +4,94 @@ from .clocks import set_custom_lmclks
 from .overlay_task import OverlayTask
 from .rfdc_type import RfDcType
 
-# Basic logging configuration
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 class RfdcTask(OverlayTask):
+    """Task for configuring RF data converters."""
+
     def __init__(self, overlay):
+        """Initialize the task with the given overlay."""
         super().__init__(overlay, name="RfdcTask")
         self.rf = self.ol.usp_rf_data_converter
 
     def run(self):
-        # Get board name
-        # board_name = os.environ['BOARD']
-
-        # Start up LMX clock
+        """Run the task."""
         set_custom_lmclks()
+        # Configure DAC tiles
+        dac_pll_settings = {
+            'PLLFreq': 409.6,
+            'SampleFreq': 1024
+        }
+        dac_mixer_settings = {
+            'CoarseMixFreq': xrfdc.COARSE_MIX_BYPASS,
+            'EventSource': xrfdc.EVNT_SRC_TILE,
+            'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
+            'Freq': 0,
+            'MixerMode': xrfdc.MIXER_MODE_C2R,
+            'MixerType': xrfdc.MIXER_TYPE_FINE,
+            'PhaseOffset': 0
+        }
+        self.configure_tiles(
+            self.rf.dac_tiles, 'DACTileStatus', dac_pll_settings, dac_mixer_settings)
 
-        # Configure all DACs
-        for tile_idx, tile in enumerate(self.rf.dac_tiles):
-            status = self.rf.IPStatus['DACTileStatus'][tile_idx]
+        # Configure ADC tiles
+        adc_pll_settings = {
+            'PLLFreq': 409.6,
+            'SampleFreq': 1024
+        }
+        adc_mixer_settings = {
+            'CoarseMixFreq': xrfdc.COARSE_MIX_BYPASS,
+            'EventSource': xrfdc.EVNT_SRC_TILE,
+            'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
+            'Freq': 0,
+            'MixerMode': xrfdc.MIXER_MODE_R2C,
+            'MixerType': xrfdc.MIXER_TYPE_FINE,
+            'PhaseOffset': 0
+        }
+        self.configure_tiles(
+            self.rf.adc_tiles, 'ADCTileStatus', adc_pll_settings, adc_mixer_settings)
 
+    def configure_tiles(self, tiles, tile_status_key, pll_settings, mixer_settings):
+        """Configure all tiles."""
+        for tile_idx, tile in enumerate(tiles):
+            status = self.rf.IPStatus[tile_status_key][tile_idx]
             if status['IsEnabled'] == 1:
-                # Configure DAC Tile
-                tile.DynamicPLLConfig(1, 409.6, 1024)
-                tile.SetupFIFO(True)
-                # Check DAC tile enable status
+                self.configure_tile(
+                    tile, pll_settings['PLLFreq'], pll_settings['SampleFreq'])
+                # Check tile status
                 for step in RfDcType.POWER_ON_SEQUENCE_STEPS:
                     if status['TileState'] == step['Sequence Number']:
                         if step['Sequence Number'] != 15:
-                            err_msg = f"DAC Tile {tile_idx} is NOT fully powered up! Stuck at Step: {step['State']} Description: {step['Description']}"
+                            err_msg = f"Tile {tile_idx} is NOT fully powered up! Stuck at Step: {step['State']} Description: {step['Description']}"
                             raise Exception(err_msg)
                         else:
                             logging.info(
-                                f"DAC Tile {tile_idx} is fully powered up!")
+                                f"Tile {tile_idx} is fully powered up!")
                         break
-                # Configure each DAC within a tile
-                block_mask = 0x1
-                for dac in tile.blocks:
-                    block_id = (block_mask & -
-                                block_mask).bit_length() - 1  # ctz
-                    if status['BlockStatusMask'] & block_mask != 0:
-                        dac.NyquistZone = 1
-                        dac.MixerSettings = {
-                            'CoarseMixFreq': xrfdc.COARSE_MIX_BYPASS,
-                            'EventSource': xrfdc.EVNT_SRC_TILE,
-                            'FineMixerScale': xrfdc.MIXER_SCALE_0P7,
-                            'Freq': 0,
-                            'MixerMode': xrfdc.MIXER_MODE_C2R,
-                            'MixerType': xrfdc.MIXER_TYPE_FINE,
-                            'PhaseOffset': 0.0
-                        }
-                        dac.UpdateEvent(xrfdc.EVENT_MIXER)
-                        logging.info(f"DAC block {block_id} is enabled!")
-                    else:
-                        logging.info(f"DAC block {block_id} is NOT enabled!")
-                    block_mask = block_mask << 1
+                self.configure_blocks(
+                    tile, status['BlockStatusMask'], mixer_settings)
 
-        # Configure all ADCs
-        for tile_idx, tile in enumerate(self.rf.adc_tiles):
-            status = self.rf.IPStatus['ADCTileStatus'][tile_idx]
+    def configure_tile(self, tile, pll_freq, freq_sample):
+        """Configure a single tile."""
+        tile.DynamicPLLConfig(1, pll_freq, freq_sample)
+        tile.SetupFIFO(True)
 
-            if status['IsEnabled'] == 1:
-                # Configure ADC Tile
-                tile.DynamicPLLConfig(1, 409.6, 1024)
-                tile.SetupFIFO(True)
-                # Check ADC tile enable status
-                for step in RfDcType.POWER_ON_SEQUENCE_STEPS:
-                    if status['TileState'] == step['Sequence Number']:
-                        if step['Sequence Number'] != 15:
-                            err_msg = f"ADC Tile {tile_idx} is NOT fully powered up! Stuck at Step: {step['State']} Description: {step['Description']}"
-                            raise Exception(err_msg)
-                        else:
-                            logging.info(
-                                f"ADC Tile {tile_idx} is fully powered up!")
-                        break
-                # Configure each ADC within a tile
-                block_mask = 0x1
-                for adc in tile.blocks:
-                    block_id = (block_mask & -
-                                block_mask).bit_length() - 1  # ctz
-                    if status['BlockStatusMask'] & block_mask != 0:
-                        adc.NyquistZone = 1
-                        adc.MixerSettings = {
-                            'CoarseMixFreq': xrfdc.COARSE_MIX_BYPASS,
-                            'EventSource': xrfdc.EVNT_SRC_TILE,
-                            'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
-                            'Freq': 0,
-                            'MixerMode': xrfdc.MIXER_MODE_R2C,
-                            'MixerType': xrfdc.MIXER_TYPE_FINE,
-                            'PhaseOffset': 0.0
-                        }
-                        adc.UpdateEvent(xrfdc.EVENT_MIXER)
-                        logging.info(f"ADC block {block_id} is enabled!")
-                    else:
-                        logging.info(f"ADC block {block_id} is NOT enabled!")
-                    block_mask = block_mask << 1
+    def configure_blocks(self, tile, block_status, mixer_settings):
+        """Configure all blocks within a tile."""
+        block_mask = 0x1
+        for block in tile.blocks:
+            block_id = (block_mask & -block_mask).bit_length() - 1  # ctz
+            if block_status & block_mask != 0:
+                self.configure_block(block, block_id, mixer_settings)
+            else:
+                logging.info(f"Block {block_id} is NOT enabled!")
+            block_mask = block_mask << 1
+
+    def configure_block(self, block, block_id, mixer_settings):
+        """Configure a single block."""
+        block.NyquistZone = 1
+        block.MixerSettings = mixer_settings
+        block.UpdateEvent(xrfdc.EVENT_MIXER)
+        logging.info(f"Block {block_id} is enabled!")
