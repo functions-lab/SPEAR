16e054bfa63aaf53089795d8d68cca0c9d819c2f
Wei Cheng <wei.cheng@duke.edu>
2024-04-22 20:50:00 +0000

    feat-Refine ADC DMA transfer and plotting feature
    - Set the packet size to 2/3 of FIFO size (refer to packet_size_ratio)
    so that we only pull this ratio amount of samples from FIFO for
    each DMA transfer.
    - The allocated DMA buffer shall add a margin to have a successful
    transfer.

--- rfsoc_rfdc/receiver_task.py
+++ rfsoc_rfdc/receiver_task.py
@@ -4,19 +4,39 @@ from .adc_data_plotter import AdcDataPlotter
 from pynq.lib import AxiGPIO
 # Don't skip this! You need this line of have PacketGenerator to work
 from .packet_generator import PacketGenerator
+from enum import Enum
 
 
 class ReceiverTask(OverlayTask):
+    """
+    A class representing the receiver task for RFSoC RFDC.
+
+    Attributes:
+        overlay (Overlay): The overlay object.
+        samples_per_axis_stream (int): The number of samples per axis stream.
+        fifo_size (int): The size of the FIFO.
+        packet_size_ratio (float): The ratio of the packet size to the FIFO size.
+        packet_size (int): The size of each packet.
+        plotter (AdcDataPlotter): The plotter object for ADC data.
+    """
+
     def __init__(self, overlay, samples_per_axis_stream=8, fifo_size=1024):
         super().__init__(overlay, name="ReceiverTask")
+        # Receiver datapath parameters
         self.fifo_size = fifo_size
         self.samples_per_axis_stream = samples_per_axis_stream
+        self.packet_size_ratio = 0.66
+        self.packet_size = int(self.fifo_size * self.packet_size_ratio)
+        # Initialize plotter
+        self.plotter = AdcDataPlotter()
+        self.plotter.config_title()
+
         # Hardware IPs
         self.t226_dma_ips = [
             self.ol.adc_datapath.t226_adc0.axi_dma
         ]
         self.t226_pkt_generator_ips = [
-            self.ol.adc_datapath.t226_adc0.adc_packet_generator
+            self.ol.adc_datapath.t226_adc0.adc_packet_generator_0
         ]
         self.t226_fifo_count_ips = [
             AxiGPIO(
@@ -27,25 +47,22 @@ class ReceiverTask(OverlayTask):
         self.t226_rx_channels = []
 
         for ch_idx, _ in enumerate(self.t226_dma_ips):
+            buffer_margin = 50  # Magic number! Necessary
             self.t226_rx_channels.append(
                 Real2IqRxChannel(
                     channel_id=ch_idx,
                     dma_ip=self.t226_dma_ips[ch_idx],
                     fifo_count_ip=self.t226_fifo_count_ips[ch_idx],
-                    buff_size=self.fifo_size * self.samples_per_axis_stream,
+                    buff_size=self.packet_size * self.samples_per_axis_stream + buffer_margin,
                     debug_mode=True
                 )
             )
 
-        # Initialize plotter
-        self.plotter = AdcDataPlotter()
-        self.plotter.config_title()
-
     def run(self):
+        # Set packet size for all ADC channels and enable each of them
         for pkg_gen in self.t226_pkt_generator_ips:
-            # Pull all samples from FIFO
-            pkg_gen.packetsize = (self.fifo_size - 1)
-        pkg_gen.enable()
+            pkg_gen.packetsize = self.packet_size
+            pkg_gen.enable()
 
         while True:
             # Receive data and convert to I/Q format
@@ -53,5 +70,5 @@ class ReceiverTask(OverlayTask):
             self.t226_rx_channels[0].wait()
             i_data = self.t226_rx_channels[0].i_data
             q_data = self.t226_rx_channels[0].q_data
-            # Update the plot with I/Q data
-            self.plotter.update_plot(i_data, q_data)
+            # Update the plot with I/Q data, plot only 10% of captured data
+            self.plotter.update_plot(i_data, q_data, display_ratio=0.1)
