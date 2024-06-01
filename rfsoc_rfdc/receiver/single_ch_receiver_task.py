18a747bf0089bc1ec3a0b91a21e2168c3f704a83
Wei Cheng <wei.cheng@duke.edu>
2024-05-31 21:10:59 -0400

    feat-Improve the performance of single ch receiver
    - Add multi-threading so that the plotting of time and freq domain plot
    is faster
    - Add multi-threading so that streaming samples through tcp connection
    is more reliable. Also, add reconnecting mechanism so that connection
    can be restored.

--- rfsoc_rfdc/receiver/single_ch_receiver_task.py
+++ rfsoc_rfdc/receiver/single_ch_receiver_task.py
@@ -6,6 +6,12 @@ from pynq.lib import AxiGPIO
 import numpy as np
 # Don't skip this! You need this line of have PacketGenerator to work
 from .packet_generator import PacketGenerator
+import logging
+import pickle
+from rfsoc_rfdc.rfdc_config import ZCU216_CONFIG
+import socket
+import time
+import threading
 
 
 class SingleChReceiverTask(OverlayTask):
@@ -13,14 +19,21 @@ class SingleChReceiverTask(OverlayTask):
 
     def __init__(self, overlay, samples_per_axis_stream=8, fifo_size=32768):
         super().__init__(overlay, name="SingleChReceiverTask")
+        # TCP socket
+        self.server_config = ("192.168.0.101", 1234)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Make sure the socket is reusable
+
         # Receiver datapath parameters
         self.fifo_size = fifo_size
         self.samples_per_axis_stream = samples_per_axis_stream
-        self.packet_size_ratio = 0.66
-        self.packet_size = int(self.fifo_size * self.packet_size_ratio)
+        self.packet_size = int(1e3)
         # Initialize plotters
         self.complex_plotter = ComplexSignalPlotter()
-        self.fft_plotter = FFTPlotter(sample_rate=100e6)
+        dac_samp_rate = ZCU216_CONFIG['DACSampleRate'] / \
+            ZCU216_CONFIG['DACInterpolationRate'] * 1e6
+        self.fft_plotter = FFTPlotter(sample_rate=dac_samp_rate)
 
         # Hardware IPs
         self.dma_ip = [
@@ -49,6 +62,57 @@ class SingleChReceiverTask(OverlayTask):
                 )
             )
 
+    def __del__(self):
+        self.sock.close()
+
+    def data_logging_handler(self, iq_data, file_name):
+        np.save(file_name, iq_data)
+
+    def tcp_reconnect(self, net_config):
+        new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        new_sock.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Make sure the socket is reusable
+        try:
+            new_sock.connect(net_config)
+        except socket.error as e:
+            return None
+        return new_sock
+
+    def tcp_handler(self, samples):
+        samples = np.array(samples, dtype=np.csingle)  # Convert to numpy array
+        try:
+            byte_data = pickle.dumps(samples)
+            self.sock.sendall(byte_data)
+        except socket.error as e:
+            for _ in range(5):
+                s = self.tcp_reconnect(self.server_config)
+                if s is not None:
+                    self.sock = s
+                    break
+
+    def sample_handler(self, iq_data):
+        start_t = time.time_ns()
+
+        # Time plot thd
+        plot_thd = threading.Thread(
+            target=self.complex_plotter.update_plot, args=(iq_data, 0.01))
+        # FFT plot thd
+        fft_thd = threading.Thread(
+            target=self.fft_plotter.update_plot, args=(iq_data,))
+        # Save IQ samples to file thd
+        log_thd = threading.Thread(
+            target=self.data_logging_handler, args=(iq_data, "./iq_data.npy"))
+        # TCP real/imag samples thd
+        tcp_thd = threading.Thread(target=self.tcp_handler, args=(iq_data,))
+
+        for thd in [tcp_thd, fft_thd, plot_thd]:
+            thd.start()
+        for thd in [tcp_thd, fft_thd, plot_thd]:
+            thd.join()
+
+        elapse = time.time_ns() - start_t
+        # logging.info(f"packet handler takes {elapse / 1e9} to complete")
+
     def run(self):
         # Set packet size for all ADC channels and enable each of them
         for pkg_gen in self.pkt_generator_ip:
@@ -59,9 +123,6 @@ class SingleChReceiverTask(OverlayTask):
             # Initiate DMA transfer
             self.rx_channels[0].transfer()
             self.rx_channels[0].wait()
-            # Receive iq samples
             iq_data = self.rx_channels[0].data
-            # Update the complex signal plotter
-            self.complex_plotter.update_plot(iq_data, plot_ratio=0.1)
-            # Update the FFT plotter
-            self.fft_plotter.update_plot(iq_data)
+            # IQ sample handler
+            self.sample_handler(iq_data)
