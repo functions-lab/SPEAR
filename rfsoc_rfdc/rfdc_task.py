ce8ce0b24f49a7ecc11e85dc9f661ca0e83649cb
Wei Cheng <wei.cheng@duke.edu>
2024-06-21 22:01:33 -0400

    feat-Add a separate RfdcMultiBandTask for multi-band config
    - This RfdcMultiBandTask is responsible for setting NCOs for DAC/ADC to
    the right frequency.

--- rfsoc_rfdc/rfdc_task.py
+++ rfsoc_rfdc/rfdc_task.py
@@ -13,6 +13,16 @@ class RfdcTask(OverlayTask):
         super().__init__(overlay, name="RfdcTask")
         self.rfdc = MyRFdc(overlay, debug_mode=True)
 
+    def run(self):
+        """Run the task."""
+        # Configure external PLL clocks
+        set_custom_lmclks()
+        # Configure RF data converters
+        self.rfdc.init_setup()
+
+
+class RfdcMultiBandTask(RfdcTask):
+
     def set_nco(self):
         # Additional configuration for DAC NCO
         dac_nco_mhz = ZCU216_CONFIG['DACNCO']
@@ -23,19 +33,15 @@ class RfdcTask(OverlayTask):
             dac_nco_mhz += dac_nco_offset
             self.rfdc.config_dac_nco(tile, block_id, dac_nco_mhz)
         # Additional configuration for ADC NCO
-        # adc_nco_mhz = ZCU216_CONFIG['ADCNCO']
-        # adc_nco_offset = ZCU216_CONFIG['ADCSampleRate'] / \
-        #     ZCU216_CONFIG['ADCInterpolationRate'] + 10
-        # tile = self.rfdc.adc_tiles[2]
-        # for block_id in [0, 1, 2, 3]:
-        #     adc_nco_mhz -= adc_nco_offset
-        #     self.rfdc.config_adc_nco(tile, block_id, adc_nco_mhz)
+        adc_nco_mhz = ZCU216_CONFIG['ADCNCO']
+        adc_nco_offset = ZCU216_CONFIG['ADCSampleRate'] / \
+            ZCU216_CONFIG['ADCInterpolationRate'] + 10
+        tile = self.rfdc.adc_tiles[2]
+        for block_id in [0, 1, 2, 3]:
+            adc_nco_mhz -= adc_nco_offset
+            self.rfdc.config_adc_nco(tile, block_id, adc_nco_mhz)
 
     def run(self):
-        """Run the task."""
-        # Configure external PLL clocks
-        set_custom_lmclks()
-        # Configure RF data converters
-        self.rfdc.init_setup()
+        super().run()
         # Customly set DAC/ADC NCO
         self.set_nco()
