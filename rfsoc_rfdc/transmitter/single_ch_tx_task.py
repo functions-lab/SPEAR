f133d9f6db5950bdfbcb42a0b16d968c44caa693
Wei Cheng <michael1996623@gmail.com>
2024-07-30 19:43:34 -0500

    fix-Wrong instance name for Tx PL DRAM

--- rfsoc_rfdc/transmitter/single_ch_tx_task.py
+++ rfsoc_rfdc/transmitter/single_ch_tx_task.py
@@ -34,7 +34,7 @@ class SingleChTxTask(OverlayTask):
                     channel_id=ch_idx,
                     dma_ip=self.dma_ip[ch_idx],
                     fifo_count_ip=self.fifo_count_ip[ch_idx],
-                    target_device=self.ol.ddr4_0,
+                    target_device=self.ol.ddr4_tx,
                     debug_mode=False
                 )
             )
